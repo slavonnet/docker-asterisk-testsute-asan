af1c8182eef292885628820e1e8f3b7815ef31b4
Badalyan Vyacheslav <v.badalyan@open-bs.ru>
2016-10-09 17:35:13 +0300

    Update vers

--- update.sh
+++ update.sh
@@ -3,8 +3,8 @@
 
 mkdir onbuild-pjproject_trunk-testsute_master-asterisk_master
 mkdir onbuild-pjproject_trunk-testsute_master-asterisk_13
-mkdir onbuild-pjproject_2.4.5-testsute_master-asterisk_master
-mkdir onbuild-pjproject_2.4.5-testsute_master-asterisk_13
+mkdir onbuild-pjproject_2.5.5-testsute_master-asterisk_master
+mkdir onbuild-pjproject_2.5.5-testsute_master-asterisk_13
 
 for i in onbuild*; do 
 	cp -f Dockerfile.template $i/Dockerfile
@@ -13,7 +13,7 @@ for i in onbuild*; do
 done
 
 sed -i -e s!asterisk_branch=master!asterisk_branch=13! *asterisk_13*/Dockerfile
-sed -i -e s!onbuild-pjproject_trunk-testsute_master!onbuild-pjproject_2.4.5-testsute_master! *pjproject_2.4.5*/Dockerfile
+sed -i -e s!onbuild-pjproject_trunk-testsute_master!onbuild-pjproject_2.5.5-testsute_master! *pjproject_2.5.5*/Dockerfile
 
 
 
