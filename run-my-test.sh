ebcddb9051952c46918467a712375f6e00b45a05
Badalyan Vyacheslav <v.badalyan@open-bs.ru>
2017-04-25 07:29:37 +0300

    Fix loging

--- run-my-test.sh
+++ run-my-test.sh
@@ -14,24 +14,22 @@ rm -rf /tmp/asterisk-testsuite/*
 
 	TEST_FILES_PATH=`find /tmp/asterisk-testsuite -name "full.txt" | grep "run_1/full" | sed -e "s/full.txt//" | tail -n1`
 
-	if [ `ls -1 /tmp/asterisk_asan/log_* 2>/dev/null | grep -c ""` -ne "0" ]; then
-		ASAN_FILES_PATH=`ls -1 /tmp/asterisk_asan/log_* | tail -n1`
-
-		if [ `cat $ASAN_FILES_PATH  | grep "ERROR" | grep -c "AddressSanitizer"` -ne "0" ]; then
-			ASAN_SUBTYPE=`cat $ASAN_FILES_PATH  | grep "ERROR" | grep "AddressSanitizer" | cut -d" " -f 3`
+	for asan_file in `ls -1 /tmp/asterisk_asan/log_* 2>/dev/null`; do
+		if [ `cat $asan_file  | grep "ERROR" | grep -c "AddressSanitizer"` -ne "0" ]; then
+			ASAN_SUBTYPE=`cat $asan_file  | grep "ERROR" | grep "AddressSanitizer" | cut -d" " -f 3`
 			ASAN_TYPE=address-$ASAN_SUBTYPE
 		else
 			ASAN_TYPE=leak
 		fi;
 		mkdir -p /tmp/asterisk_asan/$ASAN_TYPE/$TEST_ID
-		cp $ASAN_FILES_PATH /tmp/asterisk_asan/$ASAN_TYPE/$TEST_ID/asan.txt
-	else
-		mkdir -p /tmp/asterisk_asan/$ASAN_TYPE/$TEST_ID
-	fi;
+		cp $asan_file /tmp/asterisk_asan/$ASAN_TYPE/$TEST_ID/
+	done;
 
-	cp ${TEST_FILES_PATH}full.txt /tmp/asterisk_asan/$ASAN_TYPE/$TEST_ID/
-	cp ${TEST_FILES_PATH}messages.txt /tmp/asterisk_asan/$ASAN_TYPE/$TEST_ID/
-	cp /tmp/asterisk_asan/output.log /tmp/asterisk_asan/$ASAN_TYPE/$TEST_ID/runtest_log.txt
+	for i in `ls -1 /tmp/asterisk_asan/*/$TEST_ID`; do
+		cp ${TEST_FILES_PATH}full.txt $i/
+		cp ${TEST_FILES_PATH}messages.txt $i/
+		cp /tmp/asterisk_asan/output.log $i/runtest_log.txt
+	done;
 
 	rm -rf /tmp/asterisk_asan/output.log
 	rm -rf /tmp/asterisk_asan/log_*
