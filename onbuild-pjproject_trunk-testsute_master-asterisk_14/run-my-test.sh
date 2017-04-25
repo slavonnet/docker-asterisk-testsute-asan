58f38e18d938a5ecdc6451f20005ad0e1f025aad
Badalyan Vyacheslav <v.badalyan@open-bs.ru>
2017-04-25 03:47:34 +0300

    Fix pjproject 2.6 and asterisk 14

---
+++ onbuild-pjproject_trunk-testsute_master-asterisk_14/run-my-test.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+rm -rf /tmp/asterisk-testsuite/*
+
+./runtests.py -l | grep ") tests" | while read line; do 
+
+	TEST_ID=`echo $line | cut -d " " -f1`
+	TEST_NAME=`echo $line | cut -d " " -f2-9`
+	TEST_FILES_PATH=""
+	ASAN_TYPE="no_asan"
+	ASAN_SUBTYPE=""
+
+	./runtests.py -t $TEST_NAME --timeout=${TIMEOUT} | tee /tmp/asterisk_asan/output.log
+
+	TEST_FILES_PATH=`find /tmp/asterisk-testsuite -name "full.txt" | grep "run_1/full" | sed -e "s/full.txt//" | tail -n1`
+
+	if [ `ls -1 /tmp/asterisk_asan/log_* 2>/dev/null | grep -c ""` -ne "0" ]; then
+		ASAN_FILES_PATH=`ls -1 /tmp/asterisk_asan/log_* | tail -n1`
+
+		if [ `cat $ASAN_FILES_PATH  | grep "ERROR" | grep -c "AddressSanitizer"` -ne "0" ]; then
+			ASAN_SUBTYPE=`cat $ASAN_FILES_PATH  | grep "ERROR" | grep "AddressSanitizer" | cut -d" " -f 3`
+			ASAN_TYPE=address-$ASAN_SUBTYPE
+		else
+			ASAN_TYPE=leak
+		fi;
+		mkdir -p /tmp/asterisk_asan/$ASAN_TYPE/$TEST_ID
+		cp $ASAN_FILES_PATH /tmp/asterisk_asan/$ASAN_TYPE/$TEST_ID/asan.txt
+	else
+		mkdir -p /tmp/asterisk_asan/$ASAN_TYPE/$TEST_ID
+	fi;
+
+	cp ${TEST_FILES_PATH}full.txt /tmp/asterisk_asan/$ASAN_TYPE/$TEST_ID/
+	cp ${TEST_FILES_PATH}messages.txt /tmp/asterisk_asan/$ASAN_TYPE/$TEST_ID/
+	cp /tmp/asterisk_asan/output.log /tmp/asterisk_asan/$ASAN_TYPE/$TEST_ID/runtest_log.txt
+
+	rm -rf /tmp/asterisk_asan/output.log
+	rm -rf /tmp/asterisk_asan/log_*
+	rm -rf /var/log/asterisk/*
+
+	killall -9 asterisk
+
+	find /tmp/asterisk-testsuite -name "full.txt" -delete
+done;
+
+
--- update.sh
+++ update.sh
@@ -3,8 +3,10 @@
 
 mkdir onbuild-pjproject_trunk-testsute_master-asterisk_master
 mkdir onbuild-pjproject_trunk-testsute_master-asterisk_13
-mkdir onbuild-pjproject_2.5.5-testsute_master-asterisk_master
-mkdir onbuild-pjproject_2.5.5-testsute_master-asterisk_13
+mkdir onbuild-pjproject_trunk-testsute_master-asterisk_14
+mkdir onbuild-pjproject_2.6-testsute_master-asterisk_master
+mkdir onbuild-pjproject_2.6-testsute_master-asterisk_13
+mkdir onbuild-pjproject_2.6-testsute_master-asterisk_14
 
 for i in onbuild*; do 
 	cp -f Dockerfile.template $i/Dockerfile
@@ -14,7 +16,8 @@ for i in onbuild*; do
 done
 
 sed -i -e s!asterisk_branch=master!asterisk_branch=13! *asterisk_13*/Dockerfile
-sed -i -e s!onbuild-pjproject_trunk-testsute_master!onbuild-pjproject_2.5.5-testsute_master! *pjproject_2.5.5*/Dockerfile
+sed -i -e s!asterisk_branch=master!asterisk_branch=14! *asterisk_14*/Dockerfile
+sed -i -e s!onbuild-pjproject_trunk-testsute_master!onbuild-pjproject_2.6-testsute_master! *pjproject_2.6*/Dockerfile
 
 
 
